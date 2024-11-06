c01859912ac071402fa89533c6bc3e8809cdc70a
Kamvacodes2 <mtikasoga96@gmail.com>
2024-11-06 18:46:53 +0200

    jpaEntity creation and Repository

---
+++ src/main/java/net/javaguides/ems/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package net.javaguides.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository {
+    //JpaRepository already annotated with @Repository and @Transactional
+    //all methods are default transactional
+
+
+}
