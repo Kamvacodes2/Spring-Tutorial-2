9c66d52345adc60ab0158fb88001270a5a4d69f5
Kamvacodes2 <mtikasoga96@gmail.com>
2024-11-07 09:52:55 +0200

    Employee Add Rest Api

---
+++ src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package net.javaguides.ems.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    //Build Add Employee REST API
+    // Response entity is a generic class so we need to pass in the type in this case EmployeeDto
+    //USE Spring annotation to make into Rest API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){ //RequestBody extracts the json
+      EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+      return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
--- src/main/java/net/javaguides/ems/repository/EmployeeRepository.java
+++ src/main/java/net/javaguides/ems/repository/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package net.javaguides.ems.repository;
 
+import net.javaguides.ems.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //JpaRepository already annotated with @Repository and @Transactional
     //all methods are default transactional
+    // once this extends the EmployeeRepo will get CRUD methods to perform
+    // CRUD based operations on Database
 
 }
